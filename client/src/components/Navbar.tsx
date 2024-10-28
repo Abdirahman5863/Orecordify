d08cefd40b1154660ad4ed1a875aff2fae04e6cb
Abdirahman5863 <abdirahmanabdi5863@gmail.com>
2024-10-28 12:34:33 +0300

    added

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -12,7 +12,7 @@ const Navbar = () => {
       {/* search bar */}
       <div className='rounded-full hidden md:flex  item-center ring-[1.5px] ring-gray-300 p-2 gap-2 text-xs'>
         <Image src={'/search.png'} alt='search' width={20} height={20}/>
-        <input type="text" className='w-[200px] bg-transparent outline-none p-1 ' placeholder='Search.....' />
+        <input type="text" className='w-[200px] bg-transparent outline-none p-1 ' placeholder='Search....' />
       </div> 
       <div className='flex gap-6  items-center justify-end w-full'>
          <div >
