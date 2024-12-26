17185198c239f1625c86a1a5195231533a967984
Abdirahman5863 <abdirahmanabdi5863@gmail.com>
2024-12-26 09:05:05 +0300

    user

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 
-import React, { useEffect } from 'react'
+
 import Image from 'next/image'
 import { SignedIn, SignedOut, SignInButton, SignOutButton } from '@clerk/nextjs'
 import Link from 'next/link'
@@ -9,11 +9,7 @@ import { Button } from './ui/button'
  
 
 const Navbar = () => {
-  const saveUser = async () => { const response = await fetch('/api/auth/saveUser', { method: 'GET', });  
-  if (response.ok) { const user = await response.json(); 
-    console.log('User saved:', user); } 
-    else { console.error('Failed to save user'); } 
-  }; useEffect(() => { saveUser(); }, []);;
+ 
   return (
     <div className='flex justify-between items-center p-4  ' >
       {/* search bar */}
